ff84c2627ca7a33432ee4e602fa2cd1173abf268
Damian153 <damiancoman11@gmail.com>
2024-03-29 22:01:25 +0200

    pc

--- birckbreaker/CharacterBody2D.gd
+++ birckbreaker/CharacterBody2D.gd
@@ -2,16 +2,10 @@ extends CharacterBody2D
 
 
 const SPEED = 500.0
-const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	velocity.y=0
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
